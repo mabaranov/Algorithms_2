9fc813bc62c5cd9f87f07c4ab10ef50e69ce6650
Mikhail <mail.baranov@gmail.com>
2022-08-16 23:41:26 +0300

    Add solution BreadthFirstSearch

--- Vertex.java
+++ Vertex.java
@@ -26,18 +26,86 @@ class SimpleGraph
         vertex = new Vertex[size];
     }
 
+
+    public ArrayList<Vertex> BreadthFirstSearch(int VFrom, int VTo)
+    {
+        // Узлы задаются позициями в списке vertex.
+        // Возвращается список узлов -- путь из VFrom в VTo.
+        // Список пустой, если пути нету.
+
+        ArrayList<Vertex> path = new ArrayList<>();
+        int [] predecessors = new int[max_vertex];
+
+        if( BFS(VFrom, VTo, predecessors) )
+        {
+            int t = VTo;
+            path.add(vertex[t]);
+            while(t != VFrom)
+            {
+                path.add(vertex[predecessors[t]]);
+                t = predecessors[t];
+            }
+            Collections.reverse(path);
+        }
+
+        return path;
+    }
+
+    private boolean BFS(int VFrom, int VTo, int [] predecessors)
+    {
+        ArrayDeque<Integer> nodesPath = new ArrayDeque<>();
+
+        vertex[VFrom].Hit = true;
+        nodesPath.addLast( VFrom );
+        predecessors[VFrom] = VFrom;
+
+        if(VFrom == VTo)
+            return true;
+
+        int v2;
+
+        while( !nodesPath.isEmpty() )
+        {
+            int v1 = nodesPath.pop();
+
+            while ((v2 = getIndexUnvisitedNode(v1)) != -1)
+            {
+                vertex[v2].Hit = true;
+                nodesPath.addLast(v2);
+                predecessors[v2] = v1;
+
+                if(v2 == VTo)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private int getIndexUnvisitedNode(int n)
+    {
+        for(int i=0; i<max_vertex; i++)
+        {
+            if(m_adjacency[n][i]==1 && vertex[i].Hit == false)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
     public ArrayList<Vertex> DepthFirstSearch(int VFrom, int VTo)
     {
         // Узлы задаются позициями в списке vertex.
         // Возвращается список узлов -- путь из VFrom в VTo.
         // Список пустой, если пути нету.
         Stack<Vertex> nodesPath = new Stack<>();
-        searchNextNode(VFrom, nodesPath, VTo);
+        recursiveDFS(VFrom, nodesPath, VTo);
 
         return new ArrayList<Vertex>(nodesPath);
     }
 
-    private boolean searchNextNode(int nodeFrom, Stack<Vertex> nodesPath, int nodeTo)
+    private boolean recursiveDFS(int nodeFrom, Stack<Vertex> nodesPath, int nodeTo)
     {
         vertex[nodeFrom].Hit = true;
         nodesPath.push(vertex[nodeFrom]);
@@ -53,7 +121,7 @@ class SimpleGraph
         {
             if (m_adjacency[nodeFrom][i] == 1 && vertex[i].Hit == false)
             {
-                if (searchNextNode(i, nodesPath, nodeTo))
+                if (recursiveDFS(i, nodesPath, nodeTo))
                     return true;
             }
         }
